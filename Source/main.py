9a297d9b34f2077d3007b7d006009f1620996563
Nagnie <tranthaongan7epct@gmail.com>
2025-04-05 02:51:05 +0700

    add movement for pacman

--- Source/main.py
+++ Source/main.py
@@ -6,21 +6,62 @@ pygame.init()
 
 WIDTH = 900
 HEIGHT = 950
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards
-color = 'mediumblue'
-flicker = False
+color = 'blue'
 PI = math.pi
-player_img = []
+player_images = []
 for i in range(1, 5):
-    player_img.append(pygame.transform.scale(pygame.image.load(f'assets/pacman/{i}.png'), (45, 45)))
-    
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/pacman/{i}.png'), (45, 45)))
+blinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghosts/red.png'), (45, 45))
+pinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghosts/pink.png'), (45, 45))
+inky_img = pygame.transform.scale(pygame.image.load(f'assets/ghosts/blue.png'), (45, 45))
+clyde_img = pygame.transform.scale(pygame.image.load(f'assets/ghosts/orange.png'), (45, 45))
+spooked_img = pygame.transform.scale(pygame.image.load(f'assets/ghosts/powerup.png'), (45, 45))
+dead_img = pygame.transform.scale(pygame.image.load(f'assets/ghosts/dead.png'), (45, 45))
 player_x = 450
 player_y = 663
 direction = 0
+blinky_x = 56
+blinky_y = 58
+blinky_direction = 0
+inky_x = 440
+inky_y = 388
+inky_direction = 2
+pinky_x = 440
+pinky_y = 438
+pinky_direction = 2
+clyde_x = 440
+clyde_y = 438
+clyde_direction = 2
 counter = 0
+flicker = False
+# R, L, U, D
+turns_allowed = [False, False, False, False]
+direction_command = 0
+player_speed = 2
+score = 0
+powerup = False
+power_counter = 0
+eaten_ghost = [False, False, False, False]
+targets = [(player_x, player_y), (player_x, player_y), (player_x, player_y), (player_x, player_y)]
+blinky_dead = False
+inky_dead = False
+clyde_dead = False
+pinky_dead = False
+blinky_box = False
+inky_box = False
+clyde_box = False
+pinky_box = False
+moving = False
+ghost_speeds = [2, 2, 2, 2]
+startup_counter = 0
+lives = 3
+game_over = False
+game_won = False
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -56,14 +97,73 @@ def draw_board():
 
 def draw_pacman():
     if direction == 0:
-        screen.blit(player_img[counter // 5], (player_x, player_y))
+        screen.blit(player_images[counter // 5], (player_x, player_y))
     elif direction == 1:
-        screen.blit(pygame.transform.flip(player_img[counter // 5], True, False), (player_x, player_y))
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
     elif direction == 2:
-        screen.blit(pygame.transform.rotate(player_img[counter // 5], 90), (player_x, player_y))
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
     elif direction == 3:
-        screen.blit(pygame.transform.rotate(player_img[counter // 5], 270), (player_x, player_y))
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
 
+def check_position(centerx, centery):
+    turns = [False, False, False, False]
+    num1 = (HEIGHT - 50) // 32
+    num2 = (WIDTH // 30)
+    num3 = 15
+    # check collisions based on center x and center y of player +/- fudge number
+    if centerx // 30 < 29:
+        if direction == 0:
+            if level[centery // num1][(centerx - num3) // num2] < 3:
+                turns[1] = True
+        if direction == 1:
+            if level[centery // num1][(centerx + num3) // num2] < 3:
+                turns[0] = True
+        if direction == 2:
+            if level[(centery + num3) // num1][centerx // num2] < 3:
+                turns[3] = True
+        if direction == 3:
+            if level[(centery - num3) // num1][centerx // num2] < 3:
+                turns[2] = True
+
+        if direction == 2 or direction == 3:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num3) // num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num3) // num1][centerx // num2] < 3:
+                    turns[2] = True
+                    turns[1] = True
+                if level[centery // num1][(centerx + num2) // num2] < 3:
+                    turns[0] = True
+        if direction == 0 or direction == 1:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num1) // num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num1) // num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centery % num1 <= 18:
+                if level[centery // num1][(centerx - num3) // num2] < 3:
+                    turns[1] = True
+                if level[centery // num1][(centerx + num3) // num2] < 3:
+                    turns[0] = True
+    else:
+        turns[0] = True
+        turns[1] = True
+
+    return turns
+
+
+def move_player(play_x, play_y):
+    # r, l, u, d
+    if direction == 0 and turns_allowed[0]:
+        play_x += player_speed
+    elif direction == 1 and turns_allowed[1]:
+        play_x -= player_speed
+    elif direction == 2 and turns_allowed[2]:
+        play_y -= player_speed
+    elif direction == 3 and turns_allowed[3]:
+        play_y += player_speed
+    return play_x, play_y
+    
 run = True
 while run:
     timer.tick(fps)
@@ -73,50 +173,48 @@ while run:
             flicker = False
     else:
         counter = 0
+        flicker = True
     
     screen.fill('black')
     draw_board()
     draw_pacman()
+    center_x = player_x + 23
+    center_y = player_y + 24
+    turns_allowed = check_position(center_x, center_y)
+    player_x, player_y = move_player(player_x, player_y)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                direction = 0
-                player_y -= 5
-            if event.key == pygame.K_DOWN:
-                direction = 1
-                player_y += 5
-            if event.key == pygame.K_LEFT:
-                direction = 2
-                player_x -= 5
             if event.key == pygame.K_RIGHT:
-                direction = 3
-                player_x += 5
-            if event.key == pygame.K_SPACE:
-                flicker = not flicker
-            if event.key == pygame.K_ESCAPE:
-                run = False
-            if event.key == pygame.K_r:
-                player_x = 450
-                player_y = 663
                 direction = 0
-                flicker = False
-                counter = 0
-    if player_x < 0:
-        player_x = 0
-    if player_x > WIDTH - 45:
-        player_x = WIDTH - 45
-    if player_y < 0:
-        player_y = 0
-    if player_y > HEIGHT - 45:
-        player_y = HEIGHT - 45
-    if flicker:
-        if counter % 2 == 0:
-            screen.fill('black')
-        else:
-            screen.fill('white')
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_UP:
+                direction = 2
+            if event.key == pygame.K_DOWN:
+                direction = 3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT and direction_command == 0:
+                direction_command = direction
+            if event.key == pygame.K_LEFT and direction_command == 1:
+                direction_command = direction
+            if event.key == pygame.K_UP and direction_command == 2:
+                direction_command = direction
+            if event.key == pygame.K_DOWN and direction_command == 3:
+                direction_command = direction   
+        
+        for i in range(4):
+            if direction_command == i and turns_allowed[i]:
+                direction = i
+                direction_command = -1
+
+        if player_x > 900:
+            player_x = -47
+        elif player_x < -50:
+            player_x = 897
         
     pygame.display.flip()
 pygame.quit()
